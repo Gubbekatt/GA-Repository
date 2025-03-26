47836ae719f8056a04c8edd60109529ccd62ce16
Smile2f4u <vincent2fast4u@gmail.com>
2025-03-26 09:30:17 +0100

    Update AllTo3wBias.py

--- AllTo3wBias.py
+++ AllTo3wBias.py
@@ -17,29 +17,29 @@ class Brain:
                 self.nodes.append(node.Node(i))
                 self.nodes[i].layer = 0
             # Skapar bias
-            self.nodes.append(node.Node(4))
-            self.nodes[4].layer = 0
+            self.nodes.append(node.Node(5))
+            self.nodes[5].layer = 0
             # Skapar 3 neuroner i mellanlagret
-            for i in range(5,8):
+            for i in [6,7,8]:
                 self.nodes.append(node.Node(i))
                 self.nodes[i].layer = 1
             # Skapar bias
-            self.nodes.append(node.Node(8))
-            self.nodes[8].layer = 1
-            # Skapar output neuron
             self.nodes.append(node.Node(9))
-            self.nodes[9].layer = 2
+            self.nodes[9].layer = 1
+            # Skapar output neuron
+            self.nodes.append(node.Node(10))
+            self.nodes[10].layer = 2
 
             # Skapar connections (linjerna med vikter) till mellanlagret och bias
-            for i in range(0, 5):
-                for j in range(5,8):
+            for i in [0,1,2,3,4,5]:
+                for j in [6,7,8]:
                     self.connections.append(connection.Connection(self.nodes[i],
                                                                   self.nodes[j],
                                                                   random.uniform(-1, 1)))
             # Skapar connections (linjerna med vikter) till output neuron och bias
-            for i in range(5,9):
+            for i in [6,7,8,9]:
                 self.connections.append(connection.Connection(self.nodes[i],
-                                                              self.nodes[9],
+                                                              self.nodes[10],
                                                               random.uniform(-1, 1)))
 
     def connect_nodes(self):
@@ -61,14 +61,14 @@ class Brain:
         for i in range(0, self.inputs):
             self.nodes[i].output_value = vision[i]
 
-        self.nodes[4].output_value = 1
-        self.nodes[8].output_value = 1
+        self.nodes[5].output_value = 1
+        self.nodes[9].output_value = 1
 
         for i in range(0, len(self.net)):
             self.net[i].activate()
 
         # Få output-värde (y_hat) från output neuron
-        output_value = self.nodes[9].output_value
+        output_value = self.nodes[10].output_value
 
         # Reset input neuroner (perceptroner)
         for i in range(0, len(self.nodes)):
