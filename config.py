853784e445d543f188120b37e503487743dde41d
Abdi-coder1 <Abdirahmansudi122@gmail.com>
2025-05-19 22:22:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,8 @@ import components
 import pygame
 
 win_height = 720
-win_width = 550
+win_width = 1000
 window = pygame.display.set_mode((win_width, win_height))
 
 ground = components.Ground(win_width)
 pipes = []
-hinder = []
