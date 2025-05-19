20e3c4d832850b7b573ec8c9c90f862687a32669
Abdi-coder1 <Abdirahmansudi122@gmail.com>
2025-05-19 22:23:22 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Connection:
     def __init__(self, from_node, to_node, weight):
@@ -6,15 +7,31 @@ class Connection:
         self.to_node = to_node
         self.weight = weight
 
-    def mutate_weight(self):
-        if random.uniform(0, 1) < 0.1:
-            self.weight = random.uniform(-5, 5)
-        else:
-            self.weight += random.gauss(0, 1)/10
-            if self.weight > 5:
-                self.weight = 5
-            if self.weight < -5:
-                self.weight = -5
+    @staticmethod
+    def mutation_distribution(factor,strength): # Muterar efter täthets förderlning.
+        sigma = 2080
+        mean = 0
+        original_factor = 3750
+
+        value= factor* original_factor * (
+                1/(sigma*math.sqrt(math.pi*2)) *
+                math.exp((-1/2)*((strength-mean)/sigma)**2))
+        return value
+
+    def mutate_weight(self,strength,mutation_type):
+        mutation_factors = [1,1.7,2.4]
+        mutation_probabilities = [0.20, 0.40, 1]
+        value=random.uniform(0,1)
+
+        if value<=mutation_probabilities[mutation_type]:  # Vill mutera
+            mutation_step=Connection.mutation_distribution(mutation_factors[mutation_type],strength)
+            self.weight=random.uniform(self.weight-mutation_step,self.weight+mutation_step)
+
+        # kollar mina gränser.
+        if self.weight > 5:
+            self.weight = 5
+        if self.weight <- 5:
+            self.weight =- 5
 
     def clone(self, from_node, to_node):
         clone = Connection(from_node, to_node, self.weight)
